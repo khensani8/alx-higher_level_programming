d20d225f5c5c108aef65b249359ed540a9c0127d
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-08-08 10:06:10 -0700

    00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-for num in range(100):
-    if num != 99:
-        print("{:02}".format(num), end=",")
+for number in range(100):
+    if number == 99:
+        print("{}".format(number))
     else:
-        print("{}".format(num))
+        print("{:02}".format(number), end=", ")
 
