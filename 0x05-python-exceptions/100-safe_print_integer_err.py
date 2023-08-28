61dee1abc6b83ad0603790274065e1911e36fdda
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-08-28 16:31:12 -0700

    format corrections

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,6 +6,6 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         print()
         return True
-    except ValueError as e:
-        print("Exception:", e, file=sys.stderr)
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exc_info()[1]),file=sys.stderr)
         return False
