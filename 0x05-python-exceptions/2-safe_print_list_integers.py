ffaca6b16232a28e1c008cb350b3bb8f55589da0
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-08-28 09:09:09 -0700

    exceptions part 1

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    printed_integers = 0
+    try:
+        for i in range(x):
+            if type(my_list[i]) is int:
+                print("{:d}".format(my_list[i]), end="")
+                printed_integers += 1
+    except (IndexError, TypeError):
+        pass
+    print("")
+    return printed_integers
