af6f40280f0cb1e56d23a4f501424726dbfab2e2
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-09-04 13:55:18 -0700

    rect 7

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -3,7 +3,12 @@
 
 
 class Rectangle:
-    """Represent a rectangle."""
+    """Represent a rectangle.
+
+    Attributes:
+        number_of_instances (int): The number of Rectangle instances.
+        print_symbol (any): The symbol used for string representation.
+    """
 
     number_of_instances = 0
     print_symbol = "#"
@@ -15,6 +20,7 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
@@ -65,7 +71,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-            [rect.append('#') for j in range(self.__width)]
+            [rect.append(str(self.print_symbol)) for j in range(self.__width)]
             if i != self.__height - 1:
                 rect.append("\n")
         return ("".join(rect))
@@ -77,6 +83,6 @@ class Rectangle:
         return (rect)
 
     def __del__(self):
-        """Deletes the rectangle"""
+        """Print a message for every deletion of a Rectangle."""
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
