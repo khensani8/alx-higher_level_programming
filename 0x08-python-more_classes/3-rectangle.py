71cd2d36744ba8a23b3c8bba717d8378ab6f8c6d
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-09-04 13:01:59 -0700

    rect: 3

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -58,8 +58,7 @@ class Rectangle:
 
         rect = []
         for i in range(self.__height):
-             [rect.append('#') for j in range(self.__width)]
-             if i != self.__height - 1:
-                 rect.append("\n")
-                 return ("".join(rect))
-
+            [rect.append('#') for j in range(self.__width)]
+            if i != self.__height - 1:
+                rect.append("\n")
+        return ("".join(rect))
