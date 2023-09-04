3365c8a24b9d984ba2b2b7b4842c2ebab6633abb
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-09-04 13:36:58 -0700

    rect 6

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -16,6 +16,7 @@ class Rectangle:
         """
         self.width = width
         self.height = height
+        Rectangle.number_of_instances += 1
 
     @property
     def width(self):
@@ -76,5 +77,5 @@ class Rectangle:
 
     def __del__(self):
         """Deletes the rectangle"""
-        type(self).number_of_instances -= 1
+        Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
