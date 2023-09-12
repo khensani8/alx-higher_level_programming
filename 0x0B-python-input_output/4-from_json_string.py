b57010d9a56853380a629fbf59efad7a66139596
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-09-12 14:36:44 -0700

    pycodestyle fixes

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -2,6 +2,7 @@
 """Defines a JSOn to object function"""
 import json
 
+
 def from_json_string(my_str):
-    """returns an object (Python data structure) represented by a JSON string"""
+    """returns an object (Python data structure) represented by JSON string"""
     return json.loads(my_str)
