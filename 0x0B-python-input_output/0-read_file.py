cb6825a4e871ba3d8c16d9d73e3f34db30864a42
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-09-12 10:27:19 -0700

    pycodestyle fuxes

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,4 @@
 def read_file(filename=""):
     """reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf8") as f:
-        print(f.read(),end="")
+        print(f.read(), end="")
