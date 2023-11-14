c27443f9ea1c7707dab7edebaae5ee0fca4b1864
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-11-14 17:28:04 +0000

    occurrences

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  
+
   for (const element of list) {
     if (element === searchElement) {
       count++;
