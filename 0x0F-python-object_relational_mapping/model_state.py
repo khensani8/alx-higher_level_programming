1c3633fdb55a1c39ab62369a6d976d04dc8157fd
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-11-20 02:06:00 -0800

    test 3

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,41 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
-"""
-import sys
-from model_state import Base, State
+"""Module to define the State class and Base instance."""
+from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import Session
 
-from sqlalchemy import (create_engine)
+
+Base = declarative_base()
+
+class State(Base):
+    """Class definition of a State."""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+    # Connect to the MySQL server running on localhost at port 3306
+    engine = create_engine('mysql+mysqldb://username:password@localhost:3306/db_name', pool_pre_ping=True)
+
+    # Import the class definition before calling create_all
+    from your_file_name import State
+
+    # Create the table
     Base.metadata.create_all(engine)
+
+    # Example of using the State class
+    session = Session(engine)
+    
+    # Add a new state
+    new_state = State(name='New State')
+    session.add(new_state)
+    session.commit()
+
+    # Query and print all states
+    states = session.query(State).all()
+    for state in states:
+        print(state.id, state.name)
+
+    # Close the session
+    session.close()
