a86188578fb10de24a7f63ccea0174d874a56488
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2023-12-01 14:05:24 -0800

    addition of utf8

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -5,14 +5,13 @@
 """
 import urllib.request
 
-
 url = 'https://alx-intranet.hbtn.io/status'
 
-try:
-    with urllib.request.urlopen(url) as response:
-        content = response.read()
-        print(f"- type: {type(content)}")
-        print(f"- content: {content.decode('utf-8')}")
-        print("- utf8 content: OK$")
-except urllib.error.URLError as e:
-    print(f"Error fetching URL: {e}")
+with urllib.request.urlopen(url) as response:
+    content = response.read()
+    utf8_content = content.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type:", type(content))
+    print("\t- content:", content)
+    print("\t- utf8 content:", utf8_content)
