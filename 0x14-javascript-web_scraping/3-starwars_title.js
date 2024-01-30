a1be3414da88dc0dcb8d6713bfa05b6e55bda1b2
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2024-01-31 00:46:23 +0200

    experimental

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,16 +1,30 @@
 #!/usr/bin/node
 
 const request = require('request');
-const movieID = process.argv[2];
-const API_URL = https://swapi-api.alx-tools.com/api/films/:id;
 
-request(API_URL + movieID, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response .statusCode === 200) {
-    const repsonseJSON= JSON.parse(body);
-    console.log(responeJSON.title);
+// Check if the correct number of arguments is provided
+if (process.argv.length !== 3) {
+  console.error('Usage: node 3-starwars_title.js <movie-ID>');
+  process.exit(1);
+}
+
+// Get the movie ID from the command line argument
+const movieID = process.argv[2];
+
+// Make a GET request to the Star Wars API
+const url = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
   } else {
-  console.log('Error Code' + response.statusCode);
+    try {
+      // Parse the JSON response
+      const movieData = JSON.parse(body);
+
+      // Print the title of the movie
+      console.log(movieData.title);
+    } catch (parseError) {
+      console.error('Error parsing JSON response:', parseError);
+    }
   }
 });
