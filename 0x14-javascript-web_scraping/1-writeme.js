df1e4aa2a421da00f6619fc7f9a4a97c7077107e
Khensani Ngobeni <khensi.ngobeni@gmail.com>
2024-01-30 12:28:31 +0200

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,10 +4,6 @@ const fs = require('fs');
 const file = process.argv[2];
 const string = process.argv[3];
 
-fs.writeFile(file, string, 'utf-8', (err) => {
-  if (err) {
-    console.error(err);
-  } else {
-    console.log('File has been successfully written.');
-  }
+fs.writeFile(file, string, 'utf-8', function (err) {
+  if (err) console.log(err);
 });
